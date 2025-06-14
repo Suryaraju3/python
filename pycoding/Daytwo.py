49d9e17f2013ec84d755f6550f60495b5f409f9a
Suryaraju3 <suryaraju485@gmail.com>
2025-06-14 16:48:32 +0530

    updated daytwo

---
+++ pycoding/Daytwo.py
@@ -0,0 +1,71 @@
+import math
+
+print(math.sqrt(12)) #3.4641016151377544
+
+print(math.factorial(5))#120
+
+print(math.floor(24.7)) #24
+
+print(math.ceil(7.1)) #8
+
+print(round(7.7))#8
+print(round(7.2))#7
+
+print(math.trunc(254.56)) #254
+
+print(math.log(10)) #2.302585092994046
+
+print(math.sin(90)) #0.8939966636005579
+
+print(math.tan(60)) #0.320040389379563
+
+print(math.fmod(16,3)) #1.0
+
+print(math.exp(2)) #7.38905609893065
+
+print(math.fabs(34)) #34.0
+
+print(math.copysign(33,-5)) #-33.0
+
+print(math.gcd(23,2)) #1
+
+no=[2,5,4,3]
+print(math.prod(no)) #120
+
+print(math.cbrt(64)) #4.0
+
+print(math.erf(5)) #0.9999999999984626
+
+print(math.exp2(5)) #32.0
+
+print(math.lcm(12,5))#60
+
+print(math.modf(23)) #(0.0, 23.0)
+
+print(math.radians(54)) # 0.9424777960769379  angle measure
+
+print(math.log10(250)) #2.3979400086720375
+# print(math.log10(0)) # none
+
+print(math.log2(8)) #3.0
+
+print(math.isclose(4,4)) #True
+print(math.isclose(2,4)) #False
+
+print(math.isfinite(-1)) #True
+print(math.isfinite(-math.inf)) #False
+print(math.isqrt(12)) #3
+print(math.lgamma(33)) #81.55795945611503
+
+print(math.hypot(4)) # 4.0
+
+print(math.gamma(12)) #39916800.0
+
+print(math.fsum([1,2,4,5])) #12.0  use to list,tuple,array
+
+print(math.erfc(12)) #0.8875370839817152  return value -1 to 1.
+
+print(math.expm1(3)) #19.085536923187668 return value is floating.
+
+print(math.ldexp(2,3)) #16.0
+
