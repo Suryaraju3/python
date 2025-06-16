4542d40c06e8cc9e40052d3f7fa7129c56e82582
Suryaraju3 <suryaraju485@gmail.com>
2025-06-16 13:20:53 +0530

    updated daythree

---
+++ pycoding/Daythree.py
@@ -0,0 +1,35 @@
+s="@Sur123"
+u_count=0
+l_count=0
+s_count=0
+n_count=0
+
+if len(s)>=8:
+    for i in s:
+        if "A"<=i<="Z":
+            u_count+=1
+        elif "a"<=i<="z":
+            l_count+=1
+        elif "0"<=i<="9":
+            n_count+=1
+        else:
+            s_count+=1
+    if u_count>=2 and l_count>=2 and s_count>=1 and n_count>=1:
+        print("valid password")
+    else:
+        print("Invalid password")
+else:
+    print("Invalid password, check the length of the password")
+
+import random
+i=1
+while i<10:
+    n=int(input("choose your number:"))
+    guess=random.randint(1,10)
+    if n<guess:
+        print("lowest number")
+    elif n>guess:
+        print("highest number")
+    else:
+        print("got correctly")
+    i+=1
